630e4e00ed9d5ae4c7ad0457a0995d0486e9a83d
Karthikeshmanikandan <144477972+Karthikeshmanikandan@users.noreply.github.com>
2025-03-05 10:30:13 +0530

    Update main.tsx

--- hairstyleai/src/main.tsx
+++ hairstyleai/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './hairstyleai/App.tsx';
+import './hairstyleai/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
